fea3aff4e3d2f9b1797f1bc73811303a893586ef
Karunakar (Ruby) <revurikarna@gmail.com>
2012-03-19 13:17:21 +0530

    test bullet sample

---
+++ test_bullet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestBullet::Application.config.secret_token = 'd5be56160cbd28632705b7431e0c482cf5d71ade20c16322715e9d29e99411921e226250c7c4e00ec08fa71e238b93eedbc56261db6d2d5a04b651933c7519c5'
