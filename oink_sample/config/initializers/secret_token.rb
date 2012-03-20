23a85214b4ac7a92f350eb6646dd454a7b8eefe7
Karunakar (Ruby) <revurikarna@gmail.com>
2012-03-20 12:32:48 +0530

    minor

---
+++ oink_sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OinkSample::Application.config.secret_token = '9d1e5ff629ff8f4d7f35d061fc789eabb9ffa8f567a7fbe471716125b4f6cd150b9d653c6ea10f45afd995b2f2b10170e1ca9ad4112aa4e9d9ba2dfa77c11a47'
